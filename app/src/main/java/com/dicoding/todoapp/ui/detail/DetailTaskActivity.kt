95bd0f5e22a9a37c0e9dbb378b88e26cc19601d1
arya-fh-n <aryafikry86@gmail.com>
2022-06-19 19:22:39 +0700

    TODO 11 : Changed intent method to use livedata

--- app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskActivity.kt
+++ app/src/main/java/com/dicoding/todoapp/ui/detail/DetailTaskActivity.kt
@@ -1,11 +1,14 @@
 package com.dicoding.todoapp.ui.detail
 
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import com.dicoding.todoapp.R
-import com.dicoding.todoapp.data.Task
+import com.dicoding.todoapp.ui.ViewModelFactory
+import com.dicoding.todoapp.ui.list.TaskActivity
 import com.dicoding.todoapp.utils.TASK_ID
 
 class DetailTaskActivity : AppCompatActivity() {
@@ -16,18 +19,24 @@ class DetailTaskActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_task_detail)
 
+        val factory = ViewModelFactory.getInstance(this)
+        detailTaskViewModel = ViewModelProvider(this, factory).get(DetailTaskViewModel::class.java)
         //TODO 11 : Show detail task and implement delete action
-        val intent = intent.getParcelableExtra<Task>(TASK_ID)
+        val taskId = intent.getIntExtra(TASK_ID, 0)
+        detailTaskViewModel.setTaskId(taskId)
 
-        if (intent != null) {
-            detailTaskViewModel.setTaskId(intent.id)
-            findViewById<TextView>(R.id.title).text = intent.title
-            findViewById<TextView>(R.id.detail_ed_description).text = intent.description
-            findViewById<TextView>(R.id.detail_ed_due_date).text = intent.dueDateMillis.toString()
+        detailTaskViewModel.task.observe(this) { task ->
+            if (task != null) {
+                findViewById<TextView>(R.id.title).text = task.title
+                findViewById<TextView>(R.id.detail_ed_description).text = task.description
+                findViewById<TextView>(R.id.detail_ed_due_date).text = task.dueDateMillis.toString()
+            }
         }
 
         findViewById<Button>(R.id.btn_delete_task).setOnClickListener {
             detailTaskViewModel.deleteTask()
+            startActivity(Intent(this@DetailTaskActivity, TaskActivity::class.java))
+            finish()
         }
     }
 }

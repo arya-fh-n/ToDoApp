c93eff477995a516197b233d3f5830ff88d1ae86
arya-fh-n <aryafikry86@gmail.com>
2022-06-19 14:52:20 +0700

    TODO 1 First Pass : gradle library update and define Task entity

--- app/src/main/java/com/dicoding/todoapp/data/Task.kt
+++ app/src/main/java/com/dicoding/todoapp/data/Task.kt
@@ -1,10 +1,30 @@
 package com.dicoding.todoapp.data
 
+import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
 //TODO 1 : Define a local database table using the schema in app/schema/tasks.json
+@Entity(tableName = "task")
+@Parcelize
 data class Task(
+    @PrimaryKey(autoGenerate = true)
+    @NonNull
+    @ColumnInfo(name = "id")
     val id: Int = 0,
+
+    @ColumnInfo(name = "name")
     val title: String,
+
+    @ColumnInfo(name = "description")
     val description: String,
+
+    @ColumnInfo(name = "dueDate")
     val dueDateMillis: Long,
+
+    @ColumnInfo(name = "completed")
     val isCompleted: Boolean = false
-)
+): Parcelable
